6c099876bc820c756e22cdbc62a7b1fb2fe8cdea
nakulbh <nakulbhardwaj37@gmail.com>
2023-10-23 21:35:57 +0530

    cache

--- main.go
+++ main.go
@@ -3,22 +3,45 @@ package main
 import "fmt"
 
 type Node struct {
+	Val   string
+	Left  *Node //pointer declaration
+	Right *Node
 }
 
 type Queue struct {
+	Head   *Node
+	Tail   *Node
+	Length int
+}
+
+type Cache struct {
+	Queue Queue
+	Hash  Hash
 }
 
 type Hash map[string]*Node
 
+func NewCache() Cache {
+	return Cache{Queue: NewQueue(), Hash: Hash{}}
+
+}
+
+func NewQueue() {
+	head := &Node{}
+	tail := &Node{}
+
+	head.Right = tail
+	tail.Left = head
+
+	return Queue{Head: head, Tail: tail}
+
+}
+
 func main() {
 	fmt.Println("Start cache")
-	cache := Newcache()
+	cache := NewCache()
 	for _, word := range []string{"parrot", "tree", "patato", "apple", "dog", "cat"} {
 		cache.Check(word)
 		cache.Display()
 	}
-}
-
-func Newcache() {
-
 }
